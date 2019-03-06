10f2e23d1ad25727baa93fd257c3a0e7824967b8
SigiMaier <mail.siegfried.maier@gmail.com>
2019-03-06 23:24:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,3 +27,43 @@ console.log(a);
 var a = 'Hello World!';
 
 console.log(a);*/
+
+/*var isNew = true;
+console.log(isNew);
+isNew = 'yeha';
+console.log(isNew);
+isNew = 2;
+console.log(isNew);*/
+
+/*var a = 3 + 4 * 5;
+console.log(a);*/
+
+/*var a = 2,
+    b = 3,
+    c = 4;
+
+a = b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);*/
+
+/*var a = "1" + 2;
+console.log(a);*/
+
+//console.log(3 < 2 < 1);
+
+/*var a = 0;
+
+if (a || a === 0) {
+    console.log('Something is there.');
+}*/
+
+
+/*function greet(name) {
+    name = name || '<Your name here>';
+    console.log('Hello ' + name);
+}
+
+greet('Sigi');
+greet();*/
