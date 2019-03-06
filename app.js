3176de5cf8af81b4587631073af799a33507a836
Siegfried Maier <siegfried.maier@cohu.com>
2019-03-06 15:55:39 +0100

    First Few JavaScript Lines

---
+++ app.js
@@ -0,0 +1,29 @@
+/*var a;//  = 'Hello World!';
+
+console.log(a);
+
+if (a === undefined) {
+        console.log('a is undefined!');
+}
+else {
+        console.log('a is defined!');
+}*/
+
+/*function b(){
+    console.log('Called b!');
+}
+
+b();*/
+
+
+/*function b() {
+    console.log('Called b!');
+}
+
+b();
+
+console.log(a);
+
+var a = 'Hello World!';
+
+console.log(a);*/
