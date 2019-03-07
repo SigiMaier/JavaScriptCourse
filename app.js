63554141f7f7838ab3563bf33ade85880cc1fd29
Siegfried Maier <siegfried.maier@cohu.com>
2019-03-07 07:20:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,3 +27,61 @@ console.log(a);
 var a = 'Hello World!';
 
 console.log(a);*/
+
+/*function b() {
+    var myVar;
+    console.log(myVar);
+}
+
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+   // console.log(this.myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();*/
+
+
+/*function b() {
+    console.log(myVar);
+}
+
+function a() {
+    var myVar = 2;
+    b();
+}
+
+var myVar = 2;
+a();*/
+
+/*function a() {
+    
+    function b() {
+        console.log(myVar);
+    }
+    
+    var myVar = 2;
+    b();
+}
+
+var myVar = 1;
+a();*/
+
+
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
+}
+
+function clickHandler() {
+    console.log('click event!');
+}
+
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
